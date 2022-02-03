c42d6f04591bea4c8287a6a00fcbae83f0050982
kadavrah <abrahkadavrah@gmail.com>
2022-02-03 23:43:33 +0500

    add yahoo branch add YahooTest

--- src/test/java/com/example/test/PyramidaTest.java
+++ src/test/java/com/example/test/PyramidaTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class PyramidaTest extends WebDriverBase {
 
@@ -15,10 +16,33 @@ public class PyramidaTest extends WebDriverBase {
         WebElement broodcamst = getWebDriver().findElement(By.className("broodcamst"));
         WebElement promoText = broodcamst.findElement(By.tagName("span"));
         WebElement searchForm = getWebDriver().findElement((By.className("search_form")));
-        searchForm.sendKeys("где бля");
-        WebElement result = getWebDriver().findElement(By.id("result"));
+        searchForm.sendKeys("где");
+        WebElement result = getWebDriver().findElement(By.xpath("//*[@id=\"application_root\"]/div/div/div[1]/div/div[3]/div[2]"));
 
         Assertions.assertEquals("Упс... Сегодня у нас нечего Вам предложить в данной категории.", result.getText());
         Assertions.assertEquals("Товары со скидками", promoText.getText());
+    }
+
+    @Test
+    public void testBasket() throws InterruptedException {
+        getWebDriver().get("https://pyramida.info/");
+        WebElement dL = getWebDriver().findElement(By.className("one_item_block"));
+        Actions actionProvider = new Actions(getWebDriver());
+        actionProvider.moveToElement(dL).build().perform();
+        Thread.sleep(1000);
+        WebElement dl2 =getWebDriver().findElement(By.className("btn-success"));
+        dl2.click();
+        Thread.sleep(1000);
+        WebElement dl3 = getWebDriver().findElement(By.xpath("//span[text()='Продолжить просмотр']"));
+        dl3.click();
+        Thread.sleep(1000);
+        WebElement dl4 = getWebDriver().findElement(By.id("basket_icon"));
+        dl4.click();
+        Thread.sleep(1000);
+        WebElement dl5 = getWebDriver().findElement(By.xpath("//td[text()='Dying Light 2 Stay Human Стандартное издание (PS5)']"));
+
+        assertTrue(dl5.isDisplayed());
+
+
     }
 }
--- src/test/java/com/example/test/WebDriverBase.java
+++ src/test/java/com/example/test/WebDriverBase.java
@@ -2,12 +2,13 @@ package com.example.test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class WebDriverBase {
+public class WebDriverBase extends Assertions {
 
     private WebDriver webDriver;
 
